fe8801ec4ba8bdc68f884ca02c510d50f198cbd1
Temesgen Marie <tommr2323@gmail.com>
2025-08-21 20:22:56 +0300

    land controller updated

--- server/server.js
+++ server/server.js
@@ -13,6 +13,7 @@ const UserRoutes = require("./routes/userRoute");
 const DealRoutes = require("./routes/dealRoute");
 const PropertyRoute=require("./routes/propertyRoute");
 const app = express();
+app.set("trust proxy", 1);
 
 // Middleware
 app.use(express.json({ limit: '10kb' }));
