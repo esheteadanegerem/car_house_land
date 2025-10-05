55f2184812e6289264d415b24b1288cead5c8f39
Temesgen Marie <tommr2323@gmail.com>
2025-10-05 11:52:41 +0300

    consult route path updated

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const MachineRoutes=require("./routes/machineRoute")
 const UserRoutes = require("./routes/userRoute");
 const DealRoutes = require("./routes/dealRoute");
 const PropertyRoute=require("./routes/propertyRoute");
+const ConsultRoute=require("./routes/consultationRoute");
 const app = express();
 app.set("trust proxy", 1);
 
@@ -34,7 +35,7 @@ app.use('/api/machines', MachineRoutes);
 app.use('/api/users', UserRoutes);
 app.use('/api/deals', DealRoutes);
 app.use('/api/properties', PropertyRoute);
-
+app.use('/api/consultations', ConsultRoute);
 // Refresh token route
 app.post('/api/auth/refresh-token', async (req, res) => {
   try {
