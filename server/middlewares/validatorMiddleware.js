98db4214e6b021a3d713781e7c554697fd6c9f23
BekaluEshete <bekelueshete@gmail.com>
2025-12-31 03:13:05 +0300

    now chnged to use resend

--- server/middlewares/validatorMiddleware.js
+++ server/middlewares/validatorMiddleware.js
@@ -3,9 +3,11 @@ const { validationResult } = require('express-validator');
 const validateRequest = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
+    const errorArray = errors.array();
     return res.status(400).json({
       status: 'error',
-      errors: errors.array().map(error => ({
+      message: errorArray[0].msg, // Added top-level message for frontend compatibility
+      errors: errorArray.map(error => ({
         field: error.path || error.param,
         message: error.msg
       }))
